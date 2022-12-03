08e6b30ce794d155003d2eb7f66ad2093a99b1ab
autumn skerritt <github@skerritt.blog>
2022-12-03 19:33:46 +0000

    cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use std::env;
 
 use ares::config::Config;
 use ares::perform_cracking;
+use gethostname::gethostname;
 use log::{debug, error, trace};
 use serenity::async_trait;
 use serenity::framework::standard::macros::{command, group};
@@ -9,8 +10,6 @@ use serenity::framework::standard::{CommandResult, StandardFramework};
 use serenity::model::channel::Message;
 use serenity::model::Timestamp;
 use serenity::prelude::*;
-use gethostname::gethostname;
-
 
 use lemmeknow::Identifier;
 use serenity::utils::Colour;
@@ -191,15 +190,16 @@ async fn ciphey(ctx: &Context, msg: &Message) -> CommandResult {
     let tag_user = format!("👋 <@!{}>", user);
 
     // let ciphey_api_url = env::var("CIPHEY_URL").expect("Ciphey URL");
-    let ciphey_api_url = format!("https://pl8u5p7v00.execute-api.us-east-2.amazonaws.com/default/ciphey_lambda_api?ctext={}", message);
-    
+    let ciphey_api_url = format!(
+        "https://pl8u5p7v00.execute-api.us-east-2.amazonaws.com/default/ciphey_lambda_api?ctext={}",
+        message
+    );
+
     // Create a new reqwest client
     let client = reqwest::Client::new();
 
     // Make a GET request to the specified URL
-    let response = client.get(ciphey_api_url)
-        .send()
-        .await?;
+    let response = client.get(ciphey_api_url).send().await?;
 
     // Print the response status
     debug!("Response status: {}", response.status());
@@ -207,38 +207,40 @@ async fn ciphey(ctx: &Context, msg: &Message) -> CommandResult {
     // Read the response body
     let body = response.text().await?;
 
-    let my_string = "{\"message\": \"Internal server error\"}";
-
     if body == "{\"message\": \"Internal server error\"}" {
         let _msg = msg
-        .channel_id
-        .send_message(&ctx.http, |m| {
-            m.content(&tag_user).embed(|e| {
-                e.title("😭 Error: Your text could not be decoded")
-                    .field("You have some alternatives", "Use $ares to use Ares, use the Ciphey CLI or use the Ares CLI.", false)
-                    .footer(|f| f.text("http://discord.skerritt.blog"))
-                    // Add a timestamp for the current time
-                    // This also accepts a rfc3339 Timestamp
-                    .timestamp(Timestamp::now())
-                    .color(Colour::RED)
+            .channel_id
+            .send_message(&ctx.http, |m| {
+                m.content(&tag_user).embed(|e| {
+                    e.title("😭 Error: Your text could not be decoded")
+                        .field(
+                            "You have some alternatives",
+                            "Use $ares to use Ares, use the Ciphey CLI or use the Ares CLI.",
+                            false,
+                        )
+                        .footer(|f| f.text("http://discord.skerritt.blog"))
+                        // Add a timestamp for the current time
+                        // This also accepts a rfc3339 Timestamp
+                        .timestamp(Timestamp::now())
+                        .color(Colour::RED)
+                })
             })
-        })
-        .await?;
+            .await?;
     } else {
         let _msg = msg
-        .channel_id
-        .send_message(&ctx.http, |m| {
-            m.content(&tag_user).embed(|e| {
-                e.title("🥳 Your text has been decoded")
-                    .field("The plaintext is:", body.trim_matches('"'), false)
-                    .footer(|f| f.text("http://discord.skerritt.blog"))
-                    // Add a timestamp for the current time
-                    // This also accepts a rfc3339 Timestamp
-                    .timestamp(Timestamp::now())
-                    .color(Colour::RED)
+            .channel_id
+            .send_message(&ctx.http, |m| {
+                m.content(&tag_user).embed(|e| {
+                    e.title("🥳 Your text has been decoded")
+                        .field("The plaintext is:", body.trim_matches('"'), false)
+                        .footer(|f| f.text("http://discord.skerritt.blog"))
+                        // Add a timestamp for the current time
+                        // This also accepts a rfc3339 Timestamp
+                        .timestamp(Timestamp::now())
+                        .color(Colour::RED)
+                })
             })
-        })
-        .await?;
+            .await?;
     }
     Ok(())
 }
@@ -273,8 +275,6 @@ $ping
 
 PONG!
     ";
-    
-    
 
     msg.reply(ctx, message).await?;
     Ok(())
